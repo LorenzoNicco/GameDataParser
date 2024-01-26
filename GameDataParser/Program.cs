c84864bbf0740aadbd3b20c558e209dc3d4b908a
LorenzoNicco <lorenzoniccolai91@gmail.com>
2024-01-26 16:37:56 +0100

    Aggiungere i file di progetto.

---
+++ GameDataParser/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+
+var app = new GameDataParserApp();
+
+app.run();
+Console.ReadKey();
+
+public class GameDataParserApp
+{
+    private UserInteraction _userInteraction = new UserInteraction();
+    public void run()
+    {
+        _userInteraction.AskUserAFileName();
+
+        var fileNameFromUser = Console.ReadLine();
+
+        var fetchFile = File.ReadAllText(fileNameFromUser);
+
+        var videoGamesList = JsonSerializer.Deserialize<List<VideoGame>>(fetchFile);
+    }
+}
+
+public class UserInteraction
+{
+    public void AskUserAFileName()
+    {
+        Console.WriteLine("Enter the name of the file you want to read:");
+    }
+}
+
+public class VideoGame
+{
+    public string Title { get; set; }
+    public int ReleaseYear { get; set; }
+    public double Rating { get; set; }
+}
